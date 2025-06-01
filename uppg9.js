eb9a04c1376b98dcbf363787a804a8b8ce3d5e7d
ALYANNA KARLSSON <84023474+AnnaYla@users.noreply.github.com>
2025-06-01 04:45:43 +0200

    Update uppg9.js

--- uppg9.js
+++ uppg9.js
@@ -1,5 +1,6 @@
 
 
+
 function uppg9 (){
     // skapa en funktion som heter sort och tar in en array av siffror som argument
     // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
@@ -8,7 +9,23 @@ function uppg9 (){
 
     // anropa funktionen och skicka med en array som argument
 
+    function sort(num) {
+    for (let numbers of num) {
+        if (numbers % 2 === 0) {
+            console.log(numbers + " är jämt");
+        } else { 
+            console.log(numbers + " är udda")
+        }
+    }
+}
+
+sort ([10,11,12,13,14,15,16,17,18]);
   
 }
 
+
+uppg9();
+
 module.exports = { uppg9 };
+
+
