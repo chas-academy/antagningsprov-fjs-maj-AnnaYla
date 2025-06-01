c16ceb6ecaa827f19b0e110156d937b5223fe3b5
ALYANNA KARLSSON <84023474+AnnaYla@users.noreply.github.com>
2025-06-01 04:43:47 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -1,11 +1,19 @@
 
-function uppg5() {
+
+function uppg5(num1,num2) {
     // skriv en if-sats som jämför två tal
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
 
-
-
+    if (num1 > num2) {
+    console.log("Det första talet är större än det andra");
+    }
+    else {
+    console.log("Det andra talet är större än det första");
+    }
 }
 
+uppg5(108,95);
+
 module.exports = { uppg5 };
+
