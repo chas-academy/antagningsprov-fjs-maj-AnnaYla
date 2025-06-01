5398fd93d6ab74d446da2810137b4adbb5db10c3
ALYANNA KARLSSON <84023474+AnnaYla@users.noreply.github.com>
2025-06-01 04:46:08 +0200

    Update uppg10.js

--- uppg10.js
+++ uppg10.js
@@ -1,4 +1,5 @@
-function uppg10(){
+
+  function uppg10() {
     const numbers = [5, 12, 7, 10, 20, 9, 11];
   
     // skapa tre arrayer - bigEven, smallOdd, other
@@ -8,10 +9,31 @@ function uppg10(){
     // other ska innehålla alla andra tal
     // returnera ett objekt med de tre arrayerna
 
-  
-    
-  
+    const bigEven = [];
+    const smallOdd = [];
+    const other = [];
+
+  for (let num of numbers) {
+    if (num > 10 && num % 2 === 0) {
+      bigEven.push(num);
+    } else if (num < 10 && num % 2 !== 0) {
+      smallOdd.push(num);
+    } else {
+      other.push(num);
+    }
   }
-  
-  module.exports = { uppg10 };
+
+  console.log("Jämna tal större än 10 är: ", bigEven.join(", "));
+  console.log("Udda tal mindre än 10 är: ", smallOdd.join(", "));
+  console.log("och resten av tal är: ", other.join(", ")); 
+
+ return {bigEven, smallOdd, other} ;
+
+ 
+}
+
+console.log(uppg10());
+
+   
+module.exports = { uppg10 };
   
