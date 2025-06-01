95f3dc90a238169686c44dd06232f9f64622f3fc
ALYANNA KARLSSON <84023474+AnnaYla@users.noreply.github.com>
2025-06-01 04:44:13 +0200

    Update uppg6.js

--- uppg6.js
+++ uppg6.js
@@ -1,11 +1,21 @@
 
 
-function uppg6(){
+
+
+function uppg6(names){
 
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
 
+    for (girlNames of names) {
+     console.log(girlNames)
+};
  
 }
 
+let names = ["Kayla","May","Taylor","Paulie"];
+
+uppg6(names);
+
 module.exports = { uppg6 };
+
